b680070489aabb68157cf0f44f78c492c1987fbc
Matheus <mtk_z@hotmail.com>
2019-02-18 23:45:33 -0300

    add rn

--- modules/mobile/index.js
+++ modules/mobile/index.js
@@ -4,7 +4,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import { App } from '@roamingalien/components';
+
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- modules/shared/src/index.tsx
+++ modules/shared/src/index.tsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+      <Text style={styles.instructions}>To get startedsdfsfsfd, edit App.tsx</Text>
       <Text style={styles.instructions}>{count}</Text>
       <Button title="increment" onPress={() => setCount(count + 1)} />
     </View>
